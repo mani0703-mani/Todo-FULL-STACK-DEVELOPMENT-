5d9ed72ccc9df44d8c0f465672601059394027cb
mani0703-mani <manikantanagireddy07@gmail.com>
2025-07-21 13:15:52 +0530

    Create Login.js

---
+++ Login.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import API from '../api';
+import { useNavigate, Link } from 'react-router-dom';
+
+export default function Login() {
+  const [form, setForm] = useState({ email: '', password: '' });
+  const navigate = useNavigate();
+
+  const login = async () => {
+    try {
+      const res = await API.post('/auth/login', form);
+      localStorage.setItem('token', res.data.token);
+      navigate('/dashboard');
+    } catch (err) {
+      alert('Login failed');
+    }
+  };
+
+  return (
+    <div className="container">
+      <h2>Login</h2>
+      <input placeholder="Email" onChange={e => setForm({ ...form, email: e.target.value })} />
+      <input placeholder="Password" type="password" onChange={e => setForm({ ...form, password: e.target.value })} />
+      <button onClick={login}>Login</button>
+      <br />
+      <Link to="/">⬅ Back to Home</Link>
+    </div>
+  );
+                                                              }
